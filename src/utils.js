0d270e307256d4377536c219f2a630152233c7f5
V-Blaze <valentine6586@gmail.com>
2022-11-02 11:47:04 +0100

    create class methods for adding items to list

--- src/utils.js
+++ src/utils.js
@@ -18,5 +18,49 @@ static getLocalStorageData = () => {
     return todoList;
 };
 
+static addBtnRemoveEvent = () => {
+    document.querySelectorAll('.trash-can').forEach((button) => button.addEventListener('click', (event) => {
+      event.preventDefault();
+    //   const { id } = button;
+  console.log(event.target.attributes.id.value, button.id)
+    }));
+};
 
+static creatTodoItemsHtml = ({description, index}) => {
+    
+    const div = document.createElement('div');
+    div.className = 'todo-item';
+    div.innerHTML = `
+      <div class="todo_detail">
+      <input type="checkbox" id="" name="" value=""> <h3 class="item">${description}</h3> <i></i>
+      </div>
+      <div>
+      <button class="trash-can" id="${index}"><i class="fa-regular fa-pen-to-square"></i></button>
+      <button class="trash-can" id="${index}"><i class="fa-solid fa-trash-can"></i></button>
+      </div>
+      `;
+  
+    return div;
+}
+
+static showTodoItems = () => {
+    const todoList = this.getLocalStorageData()
+    document.querySelector('.todo_lists').innerHTML = ``;
+    todoList.forEach((item) => {
+        document.querySelector('.todo_lists').append(this.creatTodoItemsHtml(item));
+      });
+this.addBtnRemoveEvent()
+}
+
+static addTodoItem = (description) => {
+    const todoList = this.getLocalStorageData()
+    const index = todoList.length + 1
+    console.log(todoList.length)
+    const newTodoItem = new Todo (description, index );
+
+    todoList.push(newTodoItem)
+    console.log(newTodoItem)
+    this.setLocalStorageData(todoList)
+    this.showTodoItems()
+};
 }
