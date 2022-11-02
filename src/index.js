dd815ecd90c7a5c576278f4d5bce5265543b65c1
V-Blaze <valentine6586@gmail.com>
2022-11-02 14:49:20 +0100

    fix linter error and build the javascript modules with webpack

--- src/index.js
+++ src/index.js
@@ -1,27 +1,24 @@
 import './style.css';
-import Methods from './utils.js'
+import Methods from './utils.js';
 
 const todoForm = document.getElementById('todo-form');
 const edittodoForm = document.getElementById('edit-todo-item');
 const todoFormInput = document.getElementById('todo-input');
-const editTodoFormInput = document.querySelector('.todo-edit-input')
+const editTodoFormInput = document.querySelector('.todo-edit-input');
 
 todoForm.addEventListener('submit', (e) => {
-  e.preventDefault()
-  Methods.addTodoTask(todoFormInput.value)
+  e.preventDefault();
+  Methods.addTodoTask(todoFormInput.value);
   todoFormInput.value = '';
-})
-
+});
 
 edittodoForm.addEventListener('submit', (e) => {
-  e.preventDefault()
-  let id = Number(editTodoFormInput.getAttribute('id'))
-  Methods.updateTaskInput(editTodoFormInput.value, id)
+  e.preventDefault();
+  const id = Number(editTodoFormInput.getAttribute('id'));
+  Methods.updateTaskInput(editTodoFormInput.value, id);
   editTodoFormInput.value = '';
-  document.getElementById('todo-input').style.display = 'block'
-  edittodoForm.style.display = 'none'
-})
+  document.getElementById('todo-input').style.display = 'block';
+  edittodoForm.style.display = 'none';
+});
 
-Methods.showTodoItems()
-
-
+Methods.showTodoItems();
