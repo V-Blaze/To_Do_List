5117b5807ccb7f608be5435d0d4f6c52d9bb66e4
Wassim <wassimchakib97@gmail.com>
2022-11-09 20:28:28 +0100

    Add test for delete task

--- src/utils.test.js
+++ src/utils.test.js
@@ -1,49 +1,73 @@
 /** @jest-environment jsdom */
 
-import Utility from './utils.js'
-
-
+import Utility from './utils.js';
 
 describe('Add new task', () => {
-
-    beforeEach(() => {
-        localStorage.clear();
-        jest.clearAllMocks();
-        localStorage.setItem.mockClear();
-        document.body.innerHTML = `
-        <div class="todo_lists">
-                
+  beforeEach(() => {
+    localStorage.clear();
+    jest.clearAllMocks();
+    localStorage.setItem.mockClear();
+    document.body.innerHTML = `
+        <div class="todo_lists newClass">
+        <li></li>
         </div>`;
-      });
+  });
 
-    test('Adding a new object to local storage', () => {
-        const obj = {description: 'Practice Javascript', index: 1, completed: false};
+  test('Adding a new object to local storage', () => {
+    const obj = {
+      description: 'Practice Javascript',
+      index: 1,
+      completed: false,
+    };
 
-        Utility.addTodoTask(obj.description)
-        
-        const result = Utility.getLocalStorageData()
+    Utility.addTodoTask(obj.description);
 
-        expect(result.length).toBe(1)
-        expect(localStorage.__STORE__["todoList"]).toBe(JSON.stringify([obj]));
-    })
+    const result = Utility.getLocalStorageData();
 
-    test('Adding a second object to local storage', () => {
-        const obj = {description: 'Practice Testing', index: 1, completed: false};
-        Utility.addTodoTask(obj.description)
-        
-        const result = Utility.getLocalStorageData()
+    expect(result.length).toBe(1);
+    // eslint-disable-next-line no-underscore-dangle
+    expect(localStorage.__STORE__.todoList).toBe(JSON.stringify([obj]));
+  });
 
-        expect(result.length).toBe(1)
-        expect(localStorage.setItem).toHaveBeenLastCalledWith('todoList', JSON.stringify([obj]));
-    })
+  test('Adding a second object to local storage', () => {
+    const obj = { description: 'Practice Testing', index: 1, completed: false };
+    Utility.addTodoTask(obj.description);
 
+    const result = Utility.getLocalStorageData();
 
-    test('Add new todo task to the UI', () => {
+    expect(result.length).toBe(1);
+    expect(localStorage.setItem).toHaveBeenLastCalledWith(
+      'todoList',
+      JSON.stringify([obj]),
+    );
+  });
 
-        Utility.addTodoTask('Test the code')
-        const div = document.querySelectorAll('.todo_lists');
-        expect(div.length).toBe(1)
-    })
+  test('Add new todo task to the UI', () => {
+    const div = document.querySelectorAll('.todo_lists');
+    Utility.addTodoTask('Test the code');
+    expect(div).toHaveLength(1);
+  });
+});
 
+describe('delete list item', () => {
+  beforeEach(() => {
+    localStorage.clear();
+    jest.clearAllMocks();
+    localStorage.setItem.mockClear();
+    document.body.innerHTML = '<div class="todo_lists">'
+        + '<li></li>'
+        + '</div>';
+    const obj = { description: 'Test task', completed: false, index: 1 };
+    Utility.setLocalStorageData([obj]);
+  });
 
-})
+  test('Delete task from local storage', () => {
+    const id = 0;
+
+    Utility.deleteTask(id);
+    const result = Utility.getLocalStorageData();
+    expect(result.length).toBe(0);
+    // eslint-disable-next-line no-underscore-dangle
+    expect(localStorage.__STORE__.todoList).toBe(JSON.stringify([]));
+  });
+});
